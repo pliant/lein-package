de1adaef9266b0ff9800c50442bfd91d5de1318f
Alex Hall <alexhall@revelytix.com>
2013-05-02 13:21:30 -0400

    Allow jar extensions as long as they're classified

--- src/leiningen/package/artifact.clj
+++ src/leiningen/package/artifact.clj
@@ -38,7 +38,9 @@
     (if raw-artifacts
       (let [configured (for [entry raw-artifacts] (artifactify entry))
             pomjar #{"pom" "jar"}]
-        (filter #(or (not (:classifier %)) (not (pomjar (:extension %)))) configured)))))
+        (filter #(or (and (:classifier %)
+                          (not= "pom" (:extension %)))
+                     (not (pomjar (:extension %)))) configured)))))
 
 (defn make-jar?
   [project]
